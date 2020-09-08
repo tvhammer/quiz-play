5fc0c5a17242a5edd583433cf80fa6d50cd918f9
TV <tv>
2020-09-08 14:22:04 +0300

    HR tests passed

--- src/hackerrank/projecteuler/LongestCollatzSequence.java
+++ src/hackerrank/projecteuler/LongestCollatzSequence.java
@@ -9,22 +9,20 @@ import java.util.stream.LongStream;
 public class LongestCollatzSequence {
     public static void main(String[] args) {
 //        List<Integer> nums = Arrays.asList(10);
-//        List<Integer> nums = Arrays.asList(10, 15, 20, 1000, 1000000, 4596415);
+        List<Integer> nums = Arrays.asList(10, 15, 20, 1000, 1000000, 4596415);
 
-        List<Integer> nums = IntStream.iterate(5000000, s -> s >= 100000, s -> s - 250).boxed().collect(Collectors.toList());
+//        List<Integer> nums = IntStream.iterate(5000000, s -> s >= 100000, s -> s - 250).boxed().collect(Collectors.toList());
 
-        List<Integer> lengthMap = buildCollatz(5000000);
+        int[] lengthMap = buildCollatz(5000000);
 
         for (int n : nums) {
-            System.out.println(lengthMap.get(n));
+            System.out.println(lengthMap[n]);
         }
     }
 
-    static List<Integer> buildCollatz(int max) {
-        List<Integer> cache = new ArrayList<>();
-        List<Integer> lengthMap = new ArrayList<>();
-        cache.add(0);  // to align the index with the array index
-        lengthMap.add(0); // to align the index with the array index
+    static int[] buildCollatz(int max) {
+        int[] cache = new int[5000001];
+        int[] lengthMap = new int[5000001];
         int longestVal = 0;
         int longestItem = 0;
         for (int i = 1; i <= max; i++) {
@@ -32,23 +30,23 @@ public class LongestCollatzSequence {
             int middle = 0;
             int counter = 0;
             while (res != 1) {
-                if (res <= Integer.MAX_VALUE && cache.size() > res) {
-                    middle = cache.get((int) res);
+                if (res <= 5000000 && cache[(int) res] != 0) {
+                    middle = cache[(int) res];
                     break;
                 } else {
                     counter++;
                     res = nextCollatz(res);
                 }
             }
-            cache.add(counter + middle);
+            cache[i] = counter + middle;
 
-            int current = cache.get(i);
+            int current = cache[i];
             if (current >= longestVal) {
-                lengthMap.add(i, i);
+                lengthMap[i] = i;
                 longestVal = current;
                 longestItem = i;
             } else {
-                lengthMap.add(i, longestItem);
+                lengthMap[i] = longestItem;
             }
         }
         return lengthMap;
